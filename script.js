ee2635b457d30e5279e56ad47a33e70f1e52aa4e
zackluckyf <fanning.34@gmail.com>
2016-08-19 01:36:06 -0400

    working gif carousel

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    "use strict"
+    $('video').get(0).pause();
+    $('video').get(1).pause();
+    $('video').get(2).pause();
+    $('video').get(3).pause();
+    $('video').get(4).pause();
     var figure = $(".video").hover(hoverVideo, hideVideo);
 
     function hoverVideo(e) {
